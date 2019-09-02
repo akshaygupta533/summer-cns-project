024a8f9a406c6558bb287481349b469f0bf82de7
Akshay Gupta <akshay.gupta@research.iiit.ac.in>
2019-09-03 01:04:53 +0530

    Pipeline till exrpession metrics

---
+++ RNA-seq2/reads.R
@@ -0,0 +1,6 @@
+## ----style, echo=FALSE, results='hide', message=FALSE, cache=FALSE---------
+library(BiocStyle)
+library(knitr)
+opts_chunk$set(error=FALSE, message=FALSE, warning=FALSE, cache=TRUE)
+opts_chunk$set(fig.asp=1)
+
